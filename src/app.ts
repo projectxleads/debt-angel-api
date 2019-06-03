a2ee26a0870a66d6bdf7e49cbd514465e4dd91e9
Michael <mihendricks1@gmail.com>
2019-06-03 23:38:24 +0200

    Add hello world

--- src/app.ts
+++ src/app.ts
@@ -21,7 +21,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/api", middlewareRouter);
 
 app.get("/", (req, res) => {
-  res.send("Hello world");
+  res.send("Hello world!!!");
 });
 
 const dbModel: DbModel = {
